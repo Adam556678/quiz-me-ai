736c7a712b1be3a86b17974992ef48f72f83c6ec
Adam556678 <mahmudramadan75@yahoo.com>
2025-09-22 13:50:05 +0300

    fix genrate_question function

--- question_generator.py
+++ question_generator.py
@@ -1,19 +1,21 @@
 import os
+import random 
 from langchain_groq import ChatGroq
 from langchain_core.prompts import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate
 )
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.llm import LLMChain
 from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
 from dotenv import load_dotenv
 load_dotenv()
 
 class QuestionGenerator:
-    def __init__(self, model_name, vectorDB : FAISS):
+    def __init__(self, model_name : str, vectorDB : FAISS):
 
+        self.vector_db = vectorDB
+        
         # Create the Groq LLM
         self.model = ChatGroq(
             groq_proxy=os.environ["GROQ_API_KEY"],
@@ -23,8 +25,8 @@ class QuestionGenerator:
         # create the chat prompt
         self.prompt = self.create_prompt()
 
-        # create the retriveal chain
-        self.chain = self.create_chain(vectorDB)
+        # create the LLM chain
+        self.chain = LLMChain(llm=self.llm, prompt=self.prompt) 
         
     def create_prompt(self):
         """Creates the chat prompt"""
@@ -42,24 +44,22 @@ class QuestionGenerator:
         prompt = ChatPromptTemplate.from_messages([system, human])
         return prompt
 
-    def create_chain(self, vectorDB):
-        """Create the retrieval chain"""
-        
-        # create the document chain
-        document_chain = create_stuff_documents_chain(
-            llm=self.model,
-            prompt=self.prompt
-        )
-        
-        # create the retrieval chain
-        retriever = vectorDB.as_retriever()
-        retrieval_chain = create_retrieval_chain(retriever, document_chain)
-        
-        return retrieval_chain
-        
-
     def generate_question(self):
-        """Generate questions from a given context"""
-        response = self.chain.invoke({"input" : self.prompt})
-        return response["answer"]        
+        """
+        Pulls a random chunk from the vector store and generates a question about it.
+        """
+        retriever = self.vector_db.as_retriever(search_kwargs={"k" : 5})
+
+        # get few random chunks from the document 
+        docs = retriever.get_relevant_documents("a")
+
+        # pick one random chunk
+        random_doc = random.choice(docs)
+
+        # generate a question
+        response = self.chain.invoke({"context" : random_doc.page_content})
+        
+        return response["text"].strip()
+        
+        
         
