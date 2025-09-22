d390c38fe25c55e7210b6ba236ccb7446c0ab056
Adam556678 <mahmudramadan75@yahoo.com>
2025-09-22 10:36:51 +0300

    Implement the Processor Class

---
+++ doc_processor.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import WebBaseLoader
+
+class DocProcessor:
+    def process_pdf():
+        """Process PDF given by user"""
+        pass
+
+    def process_web_page():
+        """Process web page given by user"""
+        pass
+    
+    def process_ppt():
+        """Process PowerPoint file given by user"""
+        pass
