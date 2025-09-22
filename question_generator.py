c442ca682e891897270af9d53dc35c7962904e6b
Adam556678 <mahmudramadan75@yahoo.com>
2025-09-22 13:07:18 +0300

    Implement QuestionGenerator class

--- question_generator.py
+++ question_generator.py
@@ -5,11 +5,14 @@ from langchain_core.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate
 )
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.vectorstores import FAISS
+from langchain.chains import create_retrieval_chain
 from dotenv import load_dotenv
 load_dotenv()
 
 class QuestionGenerator:
-    def __init__(self, model_name):
+    def __init__(self, model_name, vectorDB : FAISS):
 
         # Create the Groq LLM
         self.model = ChatGroq(
@@ -17,13 +20,17 @@ class QuestionGenerator:
             model=model_name
         )
         
+        # create the chat prompt
         self.prompt = self.create_prompt()
+
+        # create the retriveal chain
+        self.chain = self.create_chain(vectorDB)
         
     def create_prompt(self):
         """Creates the chat prompt"""
         system = SystemMessagePromptTemplate.from_template(
             """
-            You are a helpful tutor. Based on the following context from a document,please generate one insightful, 
+            You are a helpful tutor. Based on the following context from a document, please generate one insightful, 
             open-ended question that would test a user's understanding of the material.
             """
         )
@@ -35,6 +42,24 @@ class QuestionGenerator:
         prompt = ChatPromptTemplate.from_messages([system, human])
         return prompt
 
+    def create_chain(self, vectorDB):
+        """Create the retrieval chain"""
+        
+        # create the document chain
+        document_chain = create_stuff_documents_chain(
+            llm=self.model,
+            prompt=self.prompt
+        )
+        
+        # create the retrieval chain
+        retriever = vectorDB.as_retriever()
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        
+        return retrieval_chain
+        
+
     def generate_question(self):
-        """Generate andswer from a given context"""
-        pass
+        """Generate questions from a given context"""
+        response = self.chain.invoke({"input" : self.prompt})
+        return response["answer"]        
+        
